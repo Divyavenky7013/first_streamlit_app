8d4b191c0c9f330e29b0a2a47483e99263808350
Divyavenky7013 <137106078+Divyavenky7013@users.noreply.github.com>
2023-06-22 13:22:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,13 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberries Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.title('My Mom\'s New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado tost')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
